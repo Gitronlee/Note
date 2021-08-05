583813100a0e30997da5f97f8995e8bb2dfe0bce
gitronlee <ronlee@live.cn>
2021-08-05 23:54:30 +0800

    Z字形变换

---
+++ Leetcode/6.z-字形变换.go
@@ -0,0 +1,109 @@
+/*
+ * @Author: ronlee
+ * @Date: 2021-08-05 23:04:51
+ * @LastEditors: ronlee
+ * @LastEditTime: 2021-08-05 23:51:34
+ * @Description: file content
+ * @FilePath: \Note\Leetcode\6.z-字形变换.go
+ */
+/*
+ * @lc app=leetcode.cn id=6 lang=golang
+ *
+ * [6] Z 字形变换
+ *
+ * https://leetcode-cn.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (50.50%)
+ * Likes:    1228
+ * Dislikes: 0
+ * Total Accepted:    281.9K
+ * Total Submissions: 558K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ *
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ *
+ * string convert(string s, int numRows);
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "PAYPALISHIRING", numRows = 3
+ * 输出："PAHNAPLSIIGYIR"
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "PAYPALISHIRING", numRows = 4
+ * 输出："PINALSIGYAHRPI"
+ * 解释：
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：s = "A", numRows = 1
+ * 输出："A"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * s 由英文字母（小写和大写）、',' 和 '.' 组成
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+func convert(s string, numRows int) string {
+	//注意对string的遍历可能出错，因此转成 byte 格式
+	matrix, down, up := make([][]byte, numRows, numRows), 0, numRows-2
+	//len(s)肯定可以拿到对应字符
+	for i := 0; i != len(s); {
+		if down != numRows {
+			//byte数组增加一个byte
+			matrix[down] = append(matrix[down], byte(s[i]))
+			down++
+			i++
+		} else if up > 0 {
+			matrix[up] = append(matrix[up], byte(s[i]))
+			up--
+			i++
+		} else {
+			up = numRows - 2
+			down = 0
+		}
+	}
+	solution := make([]byte, 0, len(s))
+	for _, row := range matrix {
+		for _, item := range row {
+			solution = append(solution, item)
+		}
+	}
+	return string(solution)
+}
+
+// @lc code=end
+
